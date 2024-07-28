48e6a01e0a18f75e06c9d8161565641183f50237
Nicolas Blaisdell <nblaisdell2@gmail.com>
2024-07-28 19:29:44 -0400

    Moved tRPC to Evercent library - v2

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,9 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
-import { appRouter, createContext } from "evercent";
 
 import * as trpcExpress from "@trpc/server/adapters/express";
+import { initTRPC } from "@trpc/server";
+import { appRouter } from "evercent";
 
 const app = express();
 
@@ -19,11 +20,46 @@ app.use(
   })
 );
 
+// created for each request
+export const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+// type Context = Awaited<ReturnType<typeof createContext>>;
+
+// export const ctx = initTRPC.context<Context>().create();
+// type TContext = typeof ctx;
+
+// export const getProc = (
+//   fn: (...args: any) => Promise<EvercentResponse<any>>,
+//   mutate: boolean
+// ) => {
+//   if (mutate) {
+//     return publicProcedure
+//       .input(z.custom<Parameters<typeof fn>[0]>())
+//       .mutation(async (opts) => {
+//         const response = await fn(opts.input);
+//         if (response.err) sendErrorEmail(mutate, response, opts);
+//         return response;
+//       });
+//   } else {
+//     return publicProcedure
+//       .input(z.custom<Parameters<typeof fn>[0]>())
+//       .query(async (opts) => {
+//         const response = await fn(opts.input);
+//         if (response.err) sendErrorEmail(mutate, response, opts);
+//         return response;
+//       });
+//   }
+// };
+
 // tRPC
-const trpcMiddleware = trpcExpress.createExpressMiddleware({
-  router: appRouter,
-  createContext,
-});
-app.use("/", trpcMiddleware);
+app.use(
+  "/",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 export default app;
